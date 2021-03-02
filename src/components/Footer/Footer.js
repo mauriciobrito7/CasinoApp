b6d8a345c608daba6f9527acef90246bdd6fc41a
Mauricio <maubot01@gmail.com>
2021-03-01 21:14:50 -0400

    sticky footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h1>Footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
