667e0a174d60f2a3ae8916b14bec0348b5125e1b
Mauricio <maubot01@gmail.com>
2021-03-01 21:18:07 -0400

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Layout from "./components/AppLayout/AppLayout";
+import Table from "@material-ui/core/Table";
+import Footer from "./components/Footer/Footer";
+import UserState from "./context/User/UserState";
+import "./App.scss";
+import Button from "@material-ui/core/Button";
+import Banner from "./assets/img/casino.jpg";
+import Modal from "@material-ui/core/Modal";
+import ContentModal from "./components/ContentModal/ContentModal";
 
 function App() {
-  return <div className="App"></div>;
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className="App">
+      <UserState>
+        <Header />
+        <Layout>
+          <main>
+            <section className="banner">
+              <div
+                style={{ backgroundImage: `url('${Banner}')` }}
+                className="banner-img"
+              ></div>
+              <div className="banner-img__gradient" />
+              <Button onClick={handleOpen} className="banner-button">
+                Try it
+              </Button>
+            </section>
+            <h2>Results</h2>
+            {/* <Table></Table> */}
+            <Modal open={open} onClose={handleClose}>
+              {<ContentModal handleClose={handleClose} />}
+            </Modal>
+          </main>
+        </Layout>
+        <Footer />
+      </UserState>
+    </div>
+  );
 }
 
 export default App;
