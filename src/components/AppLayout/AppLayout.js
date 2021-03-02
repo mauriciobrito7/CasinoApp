7a6bb95d9f638b40ec34b5570c818e35656f3684
Mauricio <maubot01@gmail.com>
2021-03-02 02:48:33 -0400

    Layout

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AppLayout = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className="appLayout">{children}</div>;
 };
 
 export default AppLayout;
